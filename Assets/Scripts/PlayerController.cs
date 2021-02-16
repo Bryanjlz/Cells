585a8667fef3c9976f280a9c9fb7228d3c093a45
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2021-02-16 13:35:20 -0500

    Basic Player and Camera Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        t.position += new Vector3(horizontalMove, 0, 0);
+    }
+}
