690784d030d2ec351f0bfb2ab6f3f6e959ebb63c
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2021-02-16 15:08:11 -0500

    No Air Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,22 +12,24 @@ public class PlayerController : MonoBehaviour
 
     float jumpVelocity = 100f;
 
-
+    public LayerMask platformLayer;
     Transform t;
     Rigidbody2D rigidBody2d;
+    BoxCollider2D collider;
 
     // Start is called before the first frame update
     void Start()
     {
         t = transform;
         rigidBody2d = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
 
             jumpVelocity = 2 * jumpHeight / jumpTime;
             rigidBody2d.gravityScale = jumpVelocity / jumpTime;
@@ -36,5 +38,11 @@ public class PlayerController : MonoBehaviour
 
         rigidBody2d.position += new Vector2(horizontalMove, 0);
 
+    }
+
+    bool IsGrounded() {
+        RaycastHit2D hit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0, Vector2.down, 0.01f, platformLayer);
+        print(hit.collider);
+        return hit.collider != null;
     }
 }
