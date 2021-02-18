bd3ba3781de01cced7405a0cf69e43b460912cba
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2021-02-17 22:37:13 -0500

    jank but fix laser bug #n

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -45,6 +45,9 @@ public class Laser : MonoBehaviour
                 direction = Vector2.right;
                 break;
         }
+
+        laserBeam.GetComponent<LaserBeam>().direction = direction;
+        laserBeam.GetComponent<LaserBeam>().worldLayer = worldLayer;
         //Create Laser
         CreateLaser();
     }
@@ -69,52 +72,52 @@ public class Laser : MonoBehaviour
     RaycastHit2D BoxCast(Vector2 origin, Vector2 size, LayerMask mask) {
         RaycastHit2D hit = Physics2D.BoxCast(origin, size, 0, direction, 100f, mask);
 
-        //Setting up the points to draw the cast
-        Vector2 p1, p2, p3, p4, p5, p6, p7, p8;
-        float w = size.x * 0.5f;
-        float h = size.y * 0.5f;
-        p1 = new Vector2(-w, h);
-        p2 = new Vector2(w, h);
-        p3 = new Vector2(w, -h);
-        p4 = new Vector2(-w, -h);
+        ////Setting up the points to draw the cast
+        //Vector2 p1, p2, p3, p4, p5, p6, p7, p8;
+        //float w = size.x * 0.5f;
+        //float h = size.y * 0.5f;
+        //p1 = new Vector2(-w, h);
+        //p2 = new Vector2(w, h);
+        //p3 = new Vector2(w, -h);
+        //p4 = new Vector2(-w, -h);
 
-        Quaternion q = Quaternion.AngleAxis(0, new Vector3(0, 0, 1));
-        p1 = q * p1;
-        p2 = q * p2;
-        p3 = q * p3;
-        p4 = q * p4;
+        //Quaternion q = Quaternion.AngleAxis(0, new Vector3(0, 0, 1));
+        //p1 = q * p1;
+        //p2 = q * p2;
+        //p3 = q * p3;
+        //p4 = q * p4;
 
-        p1 += origin;
-        p2 += origin;
-        p3 += origin;
-        p4 += origin;
+        //p1 += origin;
+        //p2 += origin;
+        //p3 += origin;
+        //p4 += origin;
 
-        Vector2 realDistance = direction.normalized * 1f;
-        p5 = p1 + realDistance;
-        p6 = p2 + realDistance;
-        p7 = p3 + realDistance;
-        p8 = p4 + realDistance;
+        //Vector2 realDistance = direction.normalized * 1f;
+        //p5 = p1 + realDistance;
+        //p6 = p2 + realDistance;
+        //p7 = p3 + realDistance;
+        //p8 = p4 + realDistance;
 
 
-        //Drawing the cast
-        Color castColor = hit ? Color.red : Color.green;
-        Debug.DrawLine(p1, p2, castColor, 10000f);
-        Debug.DrawLine(p2, p3, castColor, 10000f);
-        Debug.DrawLine(p3, p4, castColor, 10000f);
-        Debug.DrawLine(p4, p1, castColor, 10000f);
+        ////Drawing the cast
+        //Color castColor = hit ? Color.red : Color.green;
+        //Debug.DrawLine(p1, p2, castColor, 10000f);
+        //Debug.DrawLine(p2, p3, castColor, 10000f);
+        //Debug.DrawLine(p3, p4, castColor, 10000f);
+        //Debug.DrawLine(p4, p1, castColor, 10000f);
 
-        Debug.DrawLine(p5, p6, castColor, 10000f);
-        Debug.DrawLine(p6, p7, castColor, 10000f);
-        Debug.DrawLine(p7, p8, castColor, 10000f);
-        Debug.DrawLine(p8, p5, castColor, 10000f);
+        //Debug.DrawLine(p5, p6, castColor, 10000f);
+        //Debug.DrawLine(p6, p7, castColor, 10000f);
+        //Debug.DrawLine(p7, p8, castColor, 10000f);
+        //Debug.DrawLine(p8, p5, castColor, 10000f);
 
-        Debug.DrawLine(p1, p5, Color.grey, 10000f);
-        Debug.DrawLine(p2, p6, Color.grey, 10000f);
-        Debug.DrawLine(p3, p7, Color.grey, 10000f);
-        Debug.DrawLine(p4, p8, Color.grey, 10000f);
-        if (hit) {
-            Debug.DrawLine(hit.point, hit.point + hit.normal.normalized * 0.2f, Color.yellow);
-        }
+        //Debug.DrawLine(p1, p5, Color.grey, 10000f);
+        //Debug.DrawLine(p2, p6, Color.grey, 10000f);
+        //Debug.DrawLine(p3, p7, Color.grey, 10000f);
+        //Debug.DrawLine(p4, p8, Color.grey, 10000f);
+        //if (hit) {
+        //    Debug.DrawLine(hit.point, hit.point + hit.normal.normalized * 0.2f, Color.yellow);
+        //}
         return hit;
         }
     }
