e45dae87899a1253f9e07b32a651a380cb4561e9
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2021-02-19 00:21:17 -0500

    Level Select Done

--- Assets/LevelSelect.cs
+++ Assets/LevelSelect.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
@@ -14,13 +15,17 @@ public class LevelSelect : MonoBehaviour
     private const int COLUMN_COUNT = 9;
 
     [SerializeField] GameObject buttonPrefab;
+    [SerializeField] Button leftArrow;
+    [SerializeField] Button rightArrow;
 
-    private List<GameObject[][]> buttons;
+    private List<GameObject> pages;
+    private int currentPage;
+
     // Start is called before the first frame update
     void Start()
     {
         List<string> q = new List<string>();
-        buttons = new List<GameObject[][]>();
+        pages = new List<GameObject>();
 
         //Load files
         DirectoryInfo dir = new DirectoryInfo("Assets/Scenes/Levels/");
@@ -38,33 +43,30 @@ public class LevelSelect : MonoBehaviour
         for (int i = 0; i < info.Length; i++) {
             if (info[i] != null && !info[i].Name.Equals("Test Level.unity") && !info[i].Name.Equals("Level Template.unity")) {
                 q.Add(info[i].Name);
+
             }
         }
 
         //Process levels into buttons
         int k = 0;
         while(q.Count != 0) {
-            print(q[0]);
-            GameObject pageParent = new GameObject("page " + k);
-            pageParent.transform.SetParent(gameObject.transform.parent);
-            pageParent.transform.localPosition = Vector2.zero;
-            GameObject[][] page = new GameObject[ROW_COUNT][];
-            for (int i = 0; i < page.Length; i++) {
-                page[i] = new GameObject[COLUMN_COUNT];
-            }
+            GameObject page = new GameObject("page " + k);
+            page.transform.SetParent(gameObject.transform.parent.GetChild(4));
+            page.transform.localPosition = Vector2.zero;
 
-            for (int i = 0; i < page.Length; i++) {
-                for (int j = 0; j < page[0].Length; j++) {
+            for (int i = 0; i < ROW_COUNT; i++) {
+                for (int j = 0; j < COLUMN_COUNT; j++) {
+
                     //Check if done
                     if (q.Count == 0) {
                         break;
                     }
 
                     //Create button
-                    page[i][j] = Instantiate(buttonPrefab);
-                    page[i][j].transform.SetParent(pageParent.transform);
-                    page[i][j].name = q[0].Substring(0, q[0].LastIndexOf(".unity"));
-                    page[i][j].transform.localPosition = new Vector2(X_START + j * BUTT_DIST, Y_START - i * BUTT_DIST);
+                    GameObject currentButton = Instantiate(buttonPrefab);
+                    currentButton.transform.SetParent(page.transform);
+                    currentButton.name = q[0].Substring(0, q[0].LastIndexOf(".unity"));
+                    currentButton.transform.localPosition = new Vector2(X_START + j * BUTT_DIST, Y_START - i * BUTT_DIST);
 
                     //Calculate level number
                     int levelNum= k * 18 + i * 9 + j + 1;
@@ -72,7 +74,7 @@ public class LevelSelect : MonoBehaviour
                     if (levelNum < 10) {
                         levelNumStr = "0" + levelNumStr;
                     }
-                    page[i][j].GetComponentInChildren<TMP_Text>().text = levelNumStr;
+                    currentButton.GetComponentInChildren<TMP_Text>().text = levelNumStr;
                     q.RemoveAt(0);
 
                 }
@@ -81,9 +83,47 @@ public class LevelSelect : MonoBehaviour
                     break;
                 }
             }
+
+            // if not first page, don't show
+            if (k != 0) {
+                page.SetActive(false);
+            }
+
+            //Increment page number
             k++;
-            buttons.Add(page);
+
+            //Add page to list
+            pages.Add(page);
         }
 
+        //Update Arrows
+        currentPage = 0;
+        print(currentPage + " " + pages.Count);
+        UpdateArrows();
+    }
+
+    public void LeftArrow () {
+        pages[currentPage].SetActive(false);
+        currentPage--;
+        pages[currentPage].SetActive(true);
+        UpdateArrows();
+    }
+
+    public void RightArrow () {
+        pages[currentPage].SetActive(false);
+        currentPage++;
+        pages[currentPage].SetActive(true);
+        UpdateArrows();
+    }
+
+    void UpdateArrows() {
+        leftArrow.interactable = true;
+        rightArrow.interactable = true;
+        if (currentPage == 0) {
+            leftArrow.interactable = false;
+        }
+        if(currentPage == pages.Count - 1) {
+            rightArrow.interactable = false;
+        }
     }
 }
