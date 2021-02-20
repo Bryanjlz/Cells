b5ce98906759d2544c167dbc2aec29ae17e5ed6d
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2021-02-19 23:38:52 -0500

    Main Menu

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene: MonoBehaviour 
+{
+    public void Load(string name) {
+        SceneManager.LoadScene(name);
+    }
+    
+    public void Load(int id) {
+        SceneManager.LoadScene(id);
+    }
+}
