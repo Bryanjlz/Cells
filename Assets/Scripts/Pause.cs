8343fad7d1ed60f0dac4dd20f1c671f1a0fdc198
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2021-02-19 21:52:40 -0500

    QoL GUI features

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
 	[SerializeField] CanvasGroup pauseScreen;
 	[SerializeField] public static bool isPaused;
+
+	//TItle stuff
+	[SerializeField] TMP_Text titleText;
+	[SerializeField] Image titleImage;
+	private bool isFading;
+
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		isPaused = false;
 		Time.timeScale = 1;
+
+		isFading = false;
+		titleText.text = SceneManager.GetActiveScene().name.ToUpperInvariant();
+		StartCoroutine(LevelTextFade());
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if (isFading && titleImage.color.a > 0) {
+			titleImage.color = new Color(1, 1, 1, titleImage.color.a - 1f/255f);
+			titleText.color = new Color(0, 0, 0, titleImage.color.a - 1f / 255f);
+			if (titleImage.color.a <= 0) {
+				titleImage.gameObject.SetActive(false);
+				titleText.gameObject.SetActive(false);
+            }
+        }
+
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			isPaused = true;
 			Time.timeScale = 0;
@@ -25,6 +47,12 @@ public class Pause : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.R)) {
 			Restart();
         }
+	}
+
+	IEnumerator LevelTextFade() {
+		yield return new WaitForSeconds(2);
+		isFading = true;
+		
 	}
 
 	public void Restart() {
