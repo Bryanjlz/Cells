cb66b089ceadaaa7463d123813b7e12462a2b01f
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2021-02-19 19:50:12 -0500

    Fixed nth laser bug

--- Assets/Scripts/LaserBeam.cs
+++ Assets/Scripts/LaserBeam.cs
@@ -8,37 +8,47 @@ public class LaserBeam : MonoBehaviour
     public PlayerController the_player;
     public Vector2 direction;
 
+    private void Start() {
+        the_player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.name == "Player") {
             //Check if there is a shield cell in direction laser is coming from
             Collider2D player = collision.gameObject.GetComponent<Collider2D>();
             RaycastHit2D hit = Physics2D.BoxCast(player.bounds.center, player.bounds.size, 0, -direction, 0.2f, 1 << 10);
-            if (hit.collider != null) {
+            if (hit.collider != null && CheckedJoined(hit.collider)) {
                 return;
             }
             collision.gameObject.GetComponent<PlayerController>().Die();
-        } else if (collision.gameObject.GetComponent<ShieldCell>() != null)
-        {
-            // Protected
-        } else if (collision.gameObject.GetComponent<Cell>() != null)
-        {
-                //Check if there is a shield cell in direction laser is coming from
-            Collider2D player = collision.gameObject.GetComponent<Collider2D>();
-            RaycastHit2D hit = Physics2D.BoxCast(player.bounds.center, player.bounds.size, 0, -direction, 1.2f, 1 << 10);
 
-            if (collision.gameObject.GetComponent<Cell>().GetComponentInParent<PlayerController>() == null)
-            {
+        } else if (collision.gameObject.GetComponent<Cell>() != null && collision.gameObject.GetComponent<ShieldCell>() == null) {
+
+            if (collision.gameObject.GetComponent<Cell>().GetComponentInParent<PlayerController>() == null) {
                 collision.gameObject.GetComponent<Cell>().death();
                 Destroy(collision.gameObject);
                 the_player.Die();
                 return;
             }
 
-            if (hit.collider != null) {
+            //Check if there is a shield cell in direction laser is coming from
+            Collider2D player = collision.gameObject.GetComponent<Collider2D>();
+            RaycastHit2D hit = Physics2D.BoxCast(player.bounds.center, player.bounds.size, 0, -direction, 1.2f, 1 << 10);
+            if (hit.collider != null && CheckedJoined(hit.collider)) {
                 return;
             }
 
             collision.gameObject.GetComponent<Cell>().GetComponentInParent<PlayerController>().Die();
         }
+    }
+
+    private bool CheckedJoined (Collider2D c) {
+        List<Collider2D> joined = the_player.colliders;
+        foreach (Collider2D collider in joined) {
+            if (collider.gameObject.Equals(c.gameObject)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
